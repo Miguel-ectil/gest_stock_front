47078de6b6ffe9da7a0f06f452bc4de72835d0ef
Miguel-ectil <ectilmiguel@gmail.com>
2025-11-02 02:23:51 -0300

    feat: [ADD] Adicionando pagina de produto

---
+++ src/app/(private)/produtos/page.tsx
@@ -0,0 +1,8 @@
+export default function ProdutosPage() {
+  return (
+    <div>
+      <h2 className="text-2xl font-semibold mb-4">Gerenciamento de Produtos</h2>
+      {/* Conteúdo da página de produtos */}
+    </div>
+  );
+}
