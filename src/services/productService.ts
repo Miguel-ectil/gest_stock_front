ec60ded28043a73ac85c2f34bfd57cbb0c3992ea
Miguel-ectil <ectilmiguel@gmail.com>
2025-11-02 02:32:08 -0300

    feat: [ADD] Adicionando service de produtos, service de listar produtos

--- src/services/productService.ts
+++ src/services/productService.ts
@@ -4,7 +4,11 @@ import { httpClient } from "../config/axios";
 const Url = "/api/products";
 
 export const ProductService = () => {
+  const listarProdutos = async (): Promise<AxiosResponse<any>> => {
+    return await httpClient.get(Url);
+  };
+    
   return {
-
+   listarProdutos
   };
 };
