92b2f098b203a03750f7ba2310931b31e5e3da6b
Miguel Ectil <2301985@SALAS.AULAS>
2025-11-05 22:03:32 -0300

    feat: [ADD] Melhorand

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -4,8 +4,8 @@ import { httpClient } from "../config/axios"
 const Url = "/api"
 
 export const AuthService = () => {
-  const login = async (data: string,): Promise<AxiosResponse<any>> => {
-    const url = `${Url}/login`
+  const login = async (data: any,): Promise<AxiosResponse<any>> => {
+    const url = `${Url}/user/login`
     const resposta: AxiosResponse<any> = await httpClient.post(url, data);
     return resposta;
   };
