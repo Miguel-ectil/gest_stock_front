5564211abccfc67f3a9656ceec6a9782829b856e
Miguel-ectil <ectilmiguel@gmail.com>
2025-11-09 02:06:07 -0300

    feat: [UPD] Corrigindo erros de build, melhorando recuperando mensagem de erro

--- src/app/(private)/dashboard/page.tsx
+++ src/app/(private)/dashboard/page.tsx
@@ -16,12 +16,6 @@ export default function DashboardPage() {
 
   return (
     <main>
-      {user?.token && (
-        <p className="text-sm text-gray-500 dark:text-gray-400">
-          Seu token: <span className="font-mono text-gray-200 dark:text-gray-600">{user.token}</span>
-        </p>
-      )}
-
       <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
         {dashboardCards.map((card) => (
           <div
